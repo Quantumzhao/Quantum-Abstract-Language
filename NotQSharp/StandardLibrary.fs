889533358e1b0e9da20a9d20d913a10022542eda
Yishan Zhao <yishanzh@umd.edu>
2021-03-18 22:46:40 -0400

    implementing standard library

--- NotQSharp/StandardLibrary.fs
+++ NotQSharp/StandardLibrary.fs
@@ -5,13 +5,14 @@ open Helper
 open DecimalMath
 open QuantumLib
 open TypeDef
+open DecimalMath
 open Microsoft.Quantum.Canon
 
 // TODO: 
 // map: the familiar map. (a' -> b') -> a' list -> b' list
-// fori: the for iteration. (int -> a' -> (b' * c')) -> a' list -> (b' list)
 // fold: the familiar fold. (a' -> b' -> a') -> a' -> b' list -> a'
-// borrow: borrow a set of qubits, do some operations, and then return the qubits back to the set
+// range: returns a range from some int to some int
+// borrow: borrows a set of qubits, do some operations, and then return the qubits back to the set
 //         (System -> a') -> System -> System * a'
 // trim: remove all the empty holes in a qubits set left by any indexing or accessing operations
 //       System -> System
@@ -29,17 +30,45 @@ let find_variable name =
     | "I" -> Some I
     | _ -> None
 
-let find_func name =
-    match name with
-    | _ -> None
+let arg_real_check a = a = 0m || a = 1m || a = -1m
 
-let find_any name =
-    match find_variable name with
-    | Some value -> Some value
-    | None -> 
-        match find_func name with
-        | Some value -> Some value
-        | None -> None
+let add interp_w_env e1 e2 =
+    let var1 = interp_w_env e1
+    let var2 = interp_w_env e2
+    match var1, var2 with
+    | Integer_Val i1, Integer_Val i2 -> Integer_Val(i1 + i2)
+    | Complex_Val(m1, a1), Complex_Val(m2, a2) -> 
+        if a1 = 0m && a2 = 0m then
+            Complex_Val (m1 + m2, 0m)
+        else // why would anyone do this
+            let argand_to_cartesian m a =
+                m * DecimalEx.Cos (a * DecimalEx.Pi), m * DecimalEx.Sin (a * DecimalEx.Pi)
+            let r1, i1 = argand_to_cartesian m1 a1
+            let r2, i2 = argand_to_cartesian m2 a2
+            let r = r1 + r2
+            let i = i1 + i2
+            let m = DecimalEx.Sqrt (r * r + i * i)
+            let a = DecimalEx.ATan (i / r) / DecimalEx.Pi
+            Complex_Val(m, a)
+    | Complex_Val(m1, a1), Integer_Val i when arg_real_check a1 -> not_implemented_err ()
+    | Integer_Val i, Complex_Val(m2, a2) when arg_real_check a2 -> not_implemented_err ()
+    | other1, other2 -> 
+        invalidArg "expression 1 or expression 2" $"cannot cast either {other1} or {other2} to integer"
+
+let to_int interp_w_env c =
+    match interp_w_env c with
+    | Complex_Val(m, a) when arg_real_check a -> int (round m)
+    | Complex_Val(_, _) -> 
+        invalidArg "complex" "imaginary part is not 0: ambiguous rounding"
+    | Integer_Val i -> i
+    | other -> invalidArg "complex" $"cannot cast {other} to integer"
+
+let to_complex interp_w_env num =
+    match interp_w_env num with
+    | Integer_Val i when i >= 0 -> Complex_Val(decimal i, 0m)
+    | Integer_Val i when i < 0 -> Complex_Val(decimal i, 1m)
+    | Complex_Val(m, a) -> Complex_Val(m, a)
+    | other -> invalidArg "number" $"cannot cast {other} to integer"
 
 let new_qubits n sim =
     // a QArray of Q# qubits
@@ -83,3 +112,14 @@ let bin_vec_space rank =
     | Integer_Val i -> flist_to_my_list (bin_vec_space_rec i)
     | _ -> invalidArg "rank" "rank must be an integer"
 
+let find_func name =
+    match name with
+    | _ -> None
+
+let find_any name =
+    match find_variable name with
+    | Some value -> Some value
+    | None -> 
+        match find_func name with
+        | Some value -> Some value
+        | None -> None
