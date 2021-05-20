587c87022ca3bc55f143bddb48cc40b85379ede9
Yishan Zhao <yishanzh@umd.edu>
2021-05-20 14:55:22 -0400

    target file is now passed in by cmd line args instead

--- NotQSharp/Program.fs
+++ NotQSharp/Program.fs
@@ -14,17 +14,20 @@ open Interpreter
 [<EntryPoint>]
 let main argv =
     let dir = Directory.GetCurrentDirectory()
-    let tokens = lexer (dir + "/../Example/high order")
-    let expr, _ = parse tokens
-    use sim = new QuantumSimulator()
-    // Console.WriteLine "Hello"
-    Console.WriteLine(pretty_print tokens)
-    Console.WriteLine(pretty_draw expr)
-    try
-        let res = interp [] sim expr
-        Console.WriteLine res
+    if argv.Length = 0 then
+        Console.WriteLine "REPL mode has not been implemented yet! "
         0
-    with
-    | e -> 
-        Console.WriteLine e.Message
-        0
+    else
+        let tokens = lexer (dir + argv.[0])
+        let expr, _ = parse tokens
+        use sim = new QuantumSimulator()
+        // Console.WriteLine(pretty_print tokens)
+        // Console.WriteLine(pretty_draw expr)
+        try
+            let res = interp [] sim expr
+            Console.WriteLine res
+            0
+        with
+        | e -> 
+            Console.WriteLine e.Message
+            0
